951785e1b384d9afe663049126db6e10323811dd
KaitoFukutani <k.fukutani@apple.local>
2019-11-04 23:15:16 +0900

    eslint他追加

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = new express.Router();
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
-  res.render('admin/login', { title: '管理者ログイン画面' });
+  res.render('admin/login', {title: '管理者ログイン画面'});
 });
 
 module.exports = router;
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = new express.Router();
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
-  res.render('user/login', { title: 'ユーザーログイン画面' });
+  res.render('user/login', {title: 'ユーザーログイン画面'});
 });
 
 module.exports = router;
