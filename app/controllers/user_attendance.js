e2cb1515b866d4062ca6e07900bb88d2ef61768c
KaitoFukutani <k.fukutani@apple.local>
2020-02-04 00:43:06 +0900

    git cache delete

--- app/controllers/user_attendance.js
+++
@@ -1,4 +0,0 @@
-const log4js = require('log4js');
-const msg = require('../logger/message');
-const systemLogger = log4js.getLogger('system');
-const UserAttendance = require('../models/').user_attendance;
